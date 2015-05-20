61591f5fa2be445cfb539c584697e9aa0508d590
zachbachiri <zachbachiri@gmail.com>
2015-05-20 03:30:12 -0400

    Grab json data for stops

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 (function(){
  	var app = angular.module('ttime-web', []);
 
- 	app.controller('MainController', function($scope, $http){
+ 	app.controller('MainController', function($scope, $http, $q){
  		var API_KEY = 'wX9NwuHnZU2ToO7GmGR9uw';
  		$scope.position = false;
+ 		$scope.route_names = ["Blue", "Orange", "Red"]; //, "Green-B", "Green-C", "Green-D", "Green-E", "741", "742", "751", "749"
+
  		if (navigator.geolocation) {
 	        navigator.geolocation.getCurrentPosition(foundLocation);
 	    } else { 
@@ -14,21 +16,37 @@
 	    	$scope.position = position;
 	        $scope.$apply();
 	        console.log($scope.position);
-			var r = "http://realtime.mbta.com/developer/api/v2/stopsbylocation?api_key=" 
+	        http://realtime.mbta.com/developer/api/v2/stopsbyroute?api_key=wX9NwuHnZU2ToO7GmGR9uw&route=931_&format=json
+			/*var r = "http://realtime.mbta.com/developer/api/v2/stopsbylocation?api_key=" 
 			        + API_KEY 
-			        + "&lat=" 
+			        + "route=" 
 			        + $scope.position.coords.latitude
 			        + "&lon=" 
 			        + $scope.position.coords.longitude
 			        + "&format=json";
+			r="http://realtime.mbta.com/developer/api/v2/stopsbyroute?api_key=wX9NwuHnZU2ToO7GmGR9uw&route=Blue&format=json"*/
 
-			$http.get(r)
-				.success(function(data, status, headers, config) {
-				  console.log(data);
-				})
-  				.error(function(data, status, headers, config) {
-  				  console.log('error')
-  				});
+			console.log(orange_stops);
+			console.log(red_stops);
+			console.log(blue_stops);
 	    }
+
+
+	    function distanceInKM(lat1,lon1,lat2,lon2) {
+		  var R = 6371; // Radius of the earth in km
+		  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+		  var dLon = deg2rad(lon2-lon1); 
+		  var a = 
+		    Math.sin(dLat/2) * Math.sin(dLat/2) +
+		    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+		    Math.sin(dLon/2) * Math.sin(dLon/2)
+		    ; 
+		  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+		  var d = R * c; // Distance in km
+		  return d;
+		}
+		function deg2rad(deg) {
+		  return deg * (Math.PI/180)
+		}
  	});
 })();
---
+++ stops/stops.js
@@ -0,0 +1,494 @@
+var blue_stops = [
+  {
+    "stop_order": "1",
+    "stop_id": "70060",
+    "stop_name": "Wonderland",
+    "parent_station": "place-wondl",
+    "parent_station_name": "Wonderland",
+    "stop_lat": "42.41342",
+    "stop_lon": "-70.991648"
+  },
+  {
+    "stop_order": "2",
+    "stop_id": "70057",
+    "stop_name": "Revere Beach - Inbound",
+    "parent_station": "place-rbmnl",
+    "parent_station_name": "Revere Beach",
+    "stop_lat": "42.40784254",
+    "stop_lon": "-70.99253321"
+  },
+  {
+    "stop_order": "3",
+    "stop_id": "70055",
+    "stop_name": "Beachmont - Inbound",
+    "parent_station": "place-bmmnl",
+    "parent_station_name": "Beachmont",
+    "stop_lat": "42.39754234",
+    "stop_lon": "-70.99231944"
+  },
+  {
+    "stop_order": "4",
+    "stop_id": "70053",
+    "stop_name": "Suffolk Downs - Inbound",
+    "parent_station": "place-sdmnl",
+    "parent_station_name": "Suffolk Downs",
+    "stop_lat": "42.39050067",
+    "stop_lon": "-70.99712259"
+  },
+  {
+    "stop_order": "5",
+    "stop_id": "70051",
+    "stop_name": "Orient Heights - Inbound",
+    "parent_station": "place-orhte",
+    "parent_station_name": "Orient Heights",
+    "stop_lat": "42.386867",
+    "stop_lon": "-71.004736"
+  },
+  {
+    "stop_order": "6",
+    "stop_id": "70049",
+    "stop_name": "Wood Island - Inbound",
+    "parent_station": "place-wimnl",
+    "parent_station_name": "Wood Island",
+    "stop_lat": "42.3796403",
+    "stop_lon": "-71.02286539"
+  },
+  {
+    "stop_order": "7",
+    "stop_id": "70047",
+    "stop_name": "Airport - Inbound",
+    "parent_station": "place-aport",
+    "parent_station_name": "Airport",
+    "stop_lat": "42.374262",
+    "stop_lon": "-71.030395"
+  },
+  {
+    "stop_order": "8",
+    "stop_id": "70045",
+    "stop_name": "Maverick - Inbound",
+    "parent_station": "place-mvbcl",
+    "parent_station_name": "Maverick",
+    "stop_lat": "42.36911856",
+    "stop_lon": "-71.03952958"
+  },
+  {
+    "stop_order": "9",
+    "stop_id": "70043",
+    "stop_name": "Aquarium - Inbound",
+    "parent_station": "place-aqucl",
+    "parent_station_name": "Aquarium",
+    "stop_lat": "42.359784",
+    "stop_lon": "-71.051652"
+  },
+  {
+    "stop_order": "10",
+    "stop_id": "70041",
+    "stop_name": "State Street - to Bowdoin",
+    "parent_station": "place-state",
+    "parent_station_name": "State Street",
+    "stop_lat": "42.358978",
+    "stop_lon": "-71.057598"
+  },
+  {
+    "stop_order": "12",
+    "stop_id": "70038",
+    "stop_name": "Bowdoin",
+    "parent_station": "place-bomnl",
+    "parent_station_name": "Bowdoin",
+    "stop_lat": "42.361365",
+    "stop_lon": "-71.062037"
+  }
+]
+
+var orange_stops = [
+  {
+    "stop_order": "1",
+    "stop_id": "70036",
+    "stop_name": "Oak Grove",
+    "parent_station": "place-ogmnl",
+    "parent_station_name": "Oak Grove",
+    "stop_lat": "42.43668",
+    "stop_lon": "-71.071097"
+  },
+  {
+    "stop_order": "2",
+    "stop_id": "70034",
+    "stop_name": "Malden - Inbound",
+    "parent_station": "place-mlmnl",
+    "parent_station_name": "Malden Center",
+    "stop_lat": "42.426632",
+    "stop_lon": "-71.07411"
+  },
+  {
+    "stop_order": "3",
+    "stop_id": "70032",
+    "stop_name": "Wellington - Inbound",
+    "parent_station": "place-welln",
+    "parent_station_name": "Wellington",
+    "stop_lat": "42.40237",
+    "stop_lon": "-71.077082"
+  },
+  {
+    "stop_order": "4",
+    "stop_id": "70278",
+    "stop_name": "Assembly - Inbound",
+    "parent_station": "place-astao",
+    "parent_station_name": "Assembly",
+    "stop_lat": "42.392811",
+    "stop_lon": "-71.077257"
+  },
+  {
+    "stop_order": "5",
+    "stop_id": "70030",
+    "stop_name": "Sullivan Square - Inbound",
+    "parent_station": "place-sull",
+    "parent_station_name": "Sullivan Square",
+    "stop_lat": "42.383975",
+    "stop_lon": "-71.076994"
+  },
+  {
+    "stop_order": "6",
+    "stop_id": "70028",
+    "stop_name": "Community College - Inbound",
+    "parent_station": "place-ccmnl",
+    "parent_station_name": "Community College",
+    "stop_lat": "42.373622",
+    "stop_lon": "-71.069533"
+  },
+  {
+    "stop_order": "7",
+    "stop_id": "70026",
+    "stop_name": "North Station - Orange Line Inbound",
+    "parent_station": "place-north",
+    "parent_station_name": "North Station",
+    "stop_lat": "42.365577",
+    "stop_lon": "-71.06129"
+  },
+  {
+    "stop_order": "8",
+    "stop_id": "70024",
+    "stop_name": "Haymarket - Orange Line Inbound",
+    "parent_station": "place-haecl",
+    "parent_station_name": "Haymarket",
+    "stop_lat": "42.363021",
+    "stop_lon": "-71.05829"
+  },
+  {
+    "stop_order": "9",
+    "stop_id": "70022",
+    "stop_name": "State Street - to Forest Hills",
+    "parent_station": "place-state",
+    "parent_station_name": "State Street",
+    "stop_lat": "42.358978",
+    "stop_lon": "-71.057598"
+  },
+  {
+    "stop_order": "10",
+    "stop_id": "70020",
+    "stop_name": "Downtown Crossing - to Forest Hills",
+    "parent_station": "place-dwnxg",
+    "parent_station_name": "Downtown Crossing",
+    "stop_lat": "42.355518",
+    "stop_lon": "-71.060225"
+  },
+  {
+    "stop_order": "11",
+    "stop_id": "70018",
+    "stop_name": "Chinatown - Outbound",
+    "parent_station": "place-chncl",
+    "parent_station_name": "Chinatown",
+    "stop_lat": "42.352547",
+    "stop_lon": "-71.062752"
+  },
+  {
+    "stop_order": "12",
+    "stop_id": "70016",
+    "stop_name": "Tufts Medical Center - Outbound",
+    "parent_station": "place-tumnl",
+    "parent_station_name": "Tufts Medical Center",
+    "stop_lat": "42.349662",
+    "stop_lon": "-71.063917"
+  },
+  {
+    "stop_order": "13",
+    "stop_id": "70014",
+    "stop_name": "Back Bay - Outbound",
+    "parent_station": "place-bbsta",
+    "parent_station_name": "Back Bay",
+    "stop_lat": "42.34735",
+    "stop_lon": "-71.075727"
+  },
+  {
+    "stop_order": "14",
+    "stop_id": "70012",
+    "stop_name": "Massachusetts Avenue - Outbound",
+    "parent_station": "place-masta",
+    "parent_station_name": "Massachusetts Ave.",
+    "stop_lat": "42.341512",
+    "stop_lon": "-71.083423"
+  },
+  {
+    "stop_order": "15",
+    "stop_id": "70010",
+    "stop_name": "Ruggles - Outbound",
+    "parent_station": "place-rugg",
+    "parent_station_name": "Ruggles",
+    "stop_lat": "42.336377",
+    "stop_lon": "-71.088961"
+  },
+  {
+    "stop_order": "16",
+    "stop_id": "70008",
+    "stop_name": "Roxbury Crossing - Outbound",
+    "parent_station": "place-rcmnl",
+    "parent_station_name": "Roxbury Crossing",
+    "stop_lat": "42.331397",
+    "stop_lon": "-71.095451"
+  },
+  {
+    "stop_order": "17",
+    "stop_id": "70006",
+    "stop_name": "Jackson Square - Outbound",
+    "parent_station": "place-jaksn",
+    "parent_station_name": "Jackson Square",
+    "stop_lat": "42.323132",
+    "stop_lon": "-71.099592"
+  },
+  {
+    "stop_order": "18",
+    "stop_id": "70004",
+    "stop_name": "Stony Brook - Outbound",
+    "parent_station": "place-sbmnl",
+    "parent_station_name": "Stony Brook",
+    "stop_lat": "42.317062",
+    "stop_lon": "-71.104248"
+  },
+  {
+    "stop_order": "19",
+    "stop_id": "70002",
+    "stop_name": "Green Street - Outbound",
+    "parent_station": "place-grnst",
+    "parent_station_name": "Green Street",
+    "stop_lat": "42.310525",
+    "stop_lon": "-71.107414"
+  },
+  {
+    "stop_order": "20",
+    "stop_id": "70001",
+    "stop_name": "Forest Hills Orange Line",
+    "parent_station": "place-forhl",
+    "parent_station_name": "Forest Hills",
+    "stop_lat": "42.300523",
+    "stop_lon": "-71.113686"
+  }
+]
+
+var red_stops = [
+  {
+    "stop_order": "1",
+    "stop_id": "70061",
+    "stop_name": "Alewife",
+    "parent_station": "place-alfcl",
+    "parent_station_name": "Alewife",
+    "stop_lat": "42.395428",
+    "stop_lon": "-71.142483"
+  },
+  {
+    "stop_order": "2",
+    "stop_id": "70063",
+    "stop_name": "Davis - Inbound",
+    "parent_station": "place-davis",
+    "parent_station_name": "Davis",
+    "stop_lat": "42.39674",
+    "stop_lon": "-71.121815"
+  },
+  {
+    "stop_order": "3",
+    "stop_id": "70065",
+    "stop_name": "Porter - Inbound",
+    "parent_station": "place-portr",
+    "parent_station_name": "Porter",
+    "stop_lat": "42.3884",
+    "stop_lon": "-71.119149"
+  },
+  {
+    "stop_order": "4",
+    "stop_id": "70067",
+    "stop_name": "Harvard - Inbound",
+    "parent_station": "place-harsq",
+    "parent_station_name": "Harvard",
+    "stop_lat": "42.373362",
+    "stop_lon": "-71.118956"
+  },
+  {
+    "stop_order": "5",
+    "stop_id": "70069",
+    "stop_name": "Central - Inbound",
+    "parent_station": "place-cntsq",
+    "parent_station_name": "Central",
+    "stop_lat": "42.365486",
+    "stop_lon": "-71.103802"
+  },
+  {
+    "stop_order": "6",
+    "stop_id": "70071",
+    "stop_name": "Kendall/MIT - Inbound",
+    "parent_station": "place-knncl",
+    "parent_station_name": "Kendall/MIT",
+    "stop_lat": "42.36249079",
+    "stop_lon": "-71.08617653"
+  },
+  {
+    "stop_order": "7",
+    "stop_id": "70073",
+    "stop_name": "Charles/MGH - Inbound",
+    "parent_station": "place-chmnl",
+    "parent_station_name": "Charles/MGH",
+    "stop_lat": "42.361166",
+    "stop_lon": "-71.070628"
+  },
+  {
+    "stop_order": "8",
+    "stop_id": "70075",
+    "stop_name": "Park Street - to Ashmont/Braintree",
+    "parent_station": "place-pktrm",
+    "parent_station_name": "Park Street",
+    "stop_lat": "42.35639457",
+    "stop_lon": "-71.0624242"
+  },
+  {
+    "stop_order": "9",
+    "stop_id": "70077",
+    "stop_name": "Downtown Crossing - to Ashmont/Braintree",
+    "parent_station": "place-dwnxg",
+    "parent_station_name": "Downtown Crossing",
+    "stop_lat": "42.355518",
+    "stop_lon": "-71.060225"
+  },
+  {
+    "stop_order": "10",
+    "stop_id": "70079",
+    "stop_name": "South Station - Outbound",
+    "parent_station": "place-sstat",
+    "parent_station_name": "South Station",
+    "stop_lat": "42.352271",
+    "stop_lon": "-71.055242"
+  },
+  {
+    "stop_order": "11",
+    "stop_id": "70081",
+    "stop_name": "Broadway - Outbound",
+    "parent_station": "place-brdwy",
+    "parent_station_name": "Broadway",
+    "stop_lat": "42.342622",
+    "stop_lon": "-71.056967"
+  },
+  {
+    "stop_order": "12",
+    "stop_id": "70083",
+    "stop_name": "Andrew - Outbound",
+    "parent_station": "place-andrw",
+    "parent_station_name": "Andrew",
+    "stop_lat": "42.330154",
+    "stop_lon": "-71.057655"
+  },
+  {
+    "stop_order": "13",
+    "stop_id": "70085",
+    "stop_name": "JFK/UMASS Ashmont - Outbound",
+    "parent_station": "place-jfk",
+    "parent_station_name": "JFK/Umass",
+    "stop_lat": "42.320685",
+    "stop_lon": "-71.052391"
+  },
+  {
+    "stop_order": "13",
+    "stop_id": "70095",
+    "stop_name": "JFK/UMASS Braintree - Outbound",
+    "parent_station": "place-jfk",
+    "parent_station_name": "JFK/Umass",
+    "stop_lat": "42.320685",
+    "stop_lon": "-71.052391"
+  },
+  {
+    "stop_order": "14",
+    "stop_id": "70087",
+    "stop_name": "Savin Hill - Outbound",
+    "parent_station": "place-shmnl",
+    "parent_station_name": "Savin Hill",
+    "stop_lat": "42.31129",
+    "stop_lon": "-71.053331"
+  },
+  {
+    "stop_order": "15",
+    "stop_id": "70089",
+    "stop_name": "Fields Corner - Outbound",
+    "parent_station": "place-fldcr",
+    "parent_station_name": "Fields Corner",
+    "stop_lat": "42.300093",
+    "stop_lon": "-71.061667"
+  },
+  {
+    "stop_order": "16",
+    "stop_id": "70091",
+    "stop_name": "Shawmut - Outbound",
+    "parent_station": "place-smmnl",
+    "parent_station_name": "Shawmut",
+    "stop_lat": "42.29312583",
+    "stop_lon": "-71.06573796"
+  },
+  {
+    "stop_order": "17",
+    "stop_id": "70093",
+    "stop_name": "Ashmont - Outbound",
+    "parent_station": "place-asmnl",
+    "parent_station_name": "Ashmont",
+    "stop_lat": "42.284652",
+    "stop_lon": "-71.064489"
+  },
+  {
+    "stop_order": "24",
+    "stop_id": "70097",
+    "stop_name": "North Quincy - Outbound",
+    "parent_station": "place-nqncy",
+    "parent_station_name": "North Quincy",
+    "stop_lat": "42.275275",
+    "stop_lon": "-71.029583"
+  },
+  {
+    "stop_order": "25",
+    "stop_id": "70099",
+    "stop_name": "Wollaston - Outbound",
+    "parent_station": "place-wlsta",
+    "parent_station_name": "Wollaston",
+    "stop_lat": "42.2665139",
+    "stop_lon": "-71.0203369"
+  },
+  {
+    "stop_order": "26",
+    "stop_id": "70101",
+    "stop_name": "Quincy Center - Outbound",
+    "parent_station": "place-qnctr",
+    "parent_station_name": "Quincy Center",
+    "stop_lat": "42.251809",
+    "stop_lon": "-71.005409"
+  },
+  {
+    "stop_order": "27",
+    "stop_id": "70103",
+    "stop_name": "Quincy Adams - Outbound",
+    "parent_station": "place-qamnl",
+    "parent_station_name": "Quincy Adams",
+    "stop_lat": "42.233391",
+    "stop_lon": "-71.007153"
+  },
+  {
+    "stop_order": "28",
+    "stop_id": "70105",
+    "stop_name": "Braintree",
+    "parent_station": "place-brntn",
+    "parent_station_name": "Braintree",
+    "stop_lat": "42.2078543",
+    "stop_lon": "-71.0011385"
+  }
+]
