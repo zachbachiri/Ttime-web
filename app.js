08abce6e06501c9b73c4622adb27d86243eaec2c
zachbachiri <zachbachiri@gmail.com>
2015-05-25 00:47:41 -0400

    Restructure stops data and calculate closest stops to location

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
  	var app = angular.module('ttime-web', []);
 
  	app.controller('MainController', function($scope, $http, $q){
- 		var API_KEY = 'wX9NwuHnZU2ToO7GmGR9uw';
- 		$scope.position = false;
- 		$scope.route_names = ["Blue", "Orange", "Red"]; //, "Green-B", "Green-C", "Green-D", "Green-E", "741", "742", "751", "749"
+ 		var API_KEY = 'wJuqWtvdIECadwe4fAl-qg';
+ 		$scope.position = null;
+ 		$scope.inbound = true;
+ 		$scope.stops = {};
 
  		if (navigator.geolocation) {
 	        navigator.geolocation.getCurrentPosition(foundLocation);
@@ -16,7 +17,6 @@
 	    	$scope.position = position;
 	        $scope.$apply();
 	        console.log($scope.position);
-	        http://realtime.mbta.com/developer/api/v2/stopsbyroute?api_key=wX9NwuHnZU2ToO7GmGR9uw&route=931_&format=json
 			/*var r = "http://realtime.mbta.com/developer/api/v2/stopsbylocation?api_key=" 
 			        + API_KEY 
 			        + "route=" 
@@ -24,29 +24,53 @@
 			        + "&lon=" 
 			        + $scope.position.coords.longitude
 			        + "&format=json";
-			r="http://realtime.mbta.com/developer/api/v2/stopsbyroute?api_key=wX9NwuHnZU2ToO7GmGR9uw&route=Blue&format=json"*/
 
-			console.log(orange_stops);
-			console.log(red_stops);
-			console.log(blue_stops);
+			r="http://realtime.mbta.com/developer/api/v2/stopsbyroute?api_key=wX9NwuHnZU2ToO7GmGR9uw&route=Blue&format=json";*/
+
+			$scope.closest_stops = getClosestStops();
 	    }
 
 
-	    function distanceInKM(lat1,lon1,lat2,lon2) {
-		  var R = 6371; // Radius of the earth in km
-		  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-		  var dLon = deg2rad(lon2-lon1); 
-		  var a = 
-		    Math.sin(dLat/2) * Math.sin(dLat/2) +
-		    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-		    Math.sin(dLon/2) * Math.sin(dLon/2)
-		    ; 
-		  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-		  var d = R * c; // Distance in km
-		  return d;
-		}
-		function deg2rad(deg) {
-		  return deg * (Math.PI/180)
-		}
+	    	function distanceInMi(lat1, lon1, lat2, lon2) {
+				var radlat1 = Math.PI * lat1/180
+				var radlat2 = Math.PI * lat2/180
+				var radlon1 = Math.PI * lon1/180
+				var radlon2 = Math.PI * lon2/180
+				var theta = lon1-lon2
+				var radtheta = Math.PI * theta/180
+				var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+				dist = Math.acos(dist)
+				dist = dist * 180/Math.PI
+				dist = dist * 60 * 1.1515
+				return dist
+			}
+			function getClosestStops(){
+				var closest_stops = {}
+				for(var x in TTIME_LINES){
+					var line_name = TTIME_LINES[x].name;
+					x = TTIME_LINES[x]["routes"];
+					closest_stops[line_name] = {};
+
+					for(var y in x){
+						y = x[y];
+						var inbound_var = $scope.inbound ? "inbound" : "outbound";
+						var closest_id = "-1";
+						var closest_distance = 1000;
+						for(var z in y[inbound_var]["stop"]){
+							z = y[inbound_var]["stop"][z];
+							var dist = distanceInMi(parseFloat($scope.position.coords.latitude),
+													parseFloat($scope.position.coords.longitude),
+													parseFloat(z.stop_lat),
+													parseFloat(z.stop_lon));
+							if(dist < closest_distance){
+								closest_id = z.stop_id;
+								closest_distance = dist;
+							}
+						}
+		    			closest_stops[line_name][y.name] = closest_id;
+					}
+				}
+				return closest_stops;
+			}
  	});
 })();
