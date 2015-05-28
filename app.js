0803efd2869de15b1be5732f6c986ee0118afbab
zachbachiri <zachbachiri@gmail.com>
2015-05-27 20:49:05 -0400

    Get data to a point of printing stop, destination and time in minutes

--- app.js
+++ app.js
@@ -7,10 +7,11 @@
  		$scope.inbound = true;
  		$scope.trips = {};
  		$scope.closest_stops = {};
- 		updateData();
- 		//$interval(updateData(), 10000); //update our data every 60 seconds
+ 		findLocation();
+ 		$interval(findLocation(), 10000); //update our data every 60 seconds
 
-	    function updateData(){
+	    function findLocation(){
+	    	console.log('in findLocation');
 	    	if (navigator.geolocation) {
 		        navigator.geolocation.getCurrentPosition(foundLocation);
 		    } else { 
@@ -20,55 +21,44 @@
 
 	    $scope.switchDirection = function(){
 	    	$scope.inbound = !$scope.inbound;
-	    	updateData();
+	    	findLocation();
 	    }
 
 	    function foundLocation(position){
 	    	$scope.position = position;
 	        $scope.$apply();
-	        console.log($scope.position);
+	        //console.log($scope.position);
 
 
 			getClosestStops();
 			console.log($scope.closest_stops);
 
+			//need to get necessary data in here, time to arrival in minutes,
+			//stop name, end stop, etc
 			angular.forEach($scope.closest_stops, function(value,key) {
+				$scope.trips[key] = {};
 				angular.forEach(value,function(stop,line_name) {
 					$http.get(predictionAPIString(stop.stop_id)).
 					  success(function(data, status, headers, config) {
-					  	console.log(data);
-					  	var trip_data = data.mode[0].route[0].direction[0].trip;
-					  	if($scope.trips[key]){
-					  		$scope.trips[key][line_name] = trip_data;
-					  	} else {
-					  		$scope.trips[key] = {};
-					  		$scope.trips[key][line_name] = trip_data;
-					  	}
+					  	//console.log(data);
+					  	$scope.trips[key][line_name] = {};
+					  	$scope.trips[key][line_name]['trips'] = data.mode[0].route[0].direction[0].trip;
+					  	$scope.trips[key][line_name]['route_name'] = line_name;
+					  	$scope.trips[key][line_name]['parent_route_name'] = key;
+					  	$scope.trips[key][line_name]['closest_stop_name'] = stop.stop_name;
+					  	angular.forEach($scope.trips[key][line_name].trips,function(trip,key) {
+					  		trip['pre_away_rounded'] = Math.ceil(parseFloat(trip['pre_away'])/60);
+					  	})
+					  	console.log($scope.trips);
 					  }).
 					  error(function(data, status, headers, config) {
 					    console.log("ERROR - data:");
 					  });
 				})
 			})
-			console.log($scope.trips);
 	    }
 
-	    function distanceInMi(lat1, lon1, lat2, lon2) {
-
-			var radlat1 = Math.PI * lat1/180
-			var radlat2 = Math.PI * lat2/180
-			var radlon1 = Math.PI * lon1/180
-			var radlon2 = Math.PI * lon2/180
-			var theta = lon1-lon2
-			var radtheta = Math.PI * theta/180
-			var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-			dist = Math.acos(dist)
-			dist = dist * 180/Math.PI
-			dist = dist * 60 * 1.1515
-			return dist
-
-		}
-		function getClosestStops(){
+	    function getClosestStops(){
 
 			for(var x in TTIME_LINES){
 				var line_name = TTIME_LINES[x].name;
@@ -98,6 +88,23 @@
 				}
 			}
 		}
+
+	    function distanceInMi(lat1, lon1, lat2, lon2) {
+
+			var radlat1 = Math.PI * lat1/180
+			var radlat2 = Math.PI * lat2/180
+			var radlon1 = Math.PI * lon1/180
+			var radlon2 = Math.PI * lon2/180
+			var theta = lon1-lon2
+			var radtheta = Math.PI * theta/180
+			var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+			dist = Math.acos(dist)
+			dist = dist * 180/Math.PI
+			dist = dist * 60 * 1.1515
+			return dist
+
+		}
+
 		function predictionAPIString(stop_id){
 			var r = "http://realtime.mbta.com/developer/api/v2/predictionsbystop?api_key=" 
 			        + API_KEY 
@@ -105,10 +112,6 @@
 			        + stop_id
 			        + "&format=json";
 			return r;
-		}
-
-		function minutesUntilTime(future_time){
-
 		}
  	});
 })();
